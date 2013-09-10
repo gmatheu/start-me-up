d47ecbc37d8b7e76cdf372e055fefb0a630a1462
Gonzalo Matheu <gmail@gonzalommj.com>
2013-09-10 13:56:16 -0300

    Adding mac keyboard info

--- mac-settings.sh
+++ mac-settings.sh
@@ -4,3 +4,5 @@ echo "Setting default Fn keys"
 # https://help.ubuntu.com/community/AppleKeyboard
 echo options hid_apple fnmode=2 | sudo tee -a /etc/modprobe.d/hid_apple.conf
 sudo update-initramfs -u -k all
+
+# http://www.rierol.net/ubuntu-12.04-LTS-macbook-pro.html
