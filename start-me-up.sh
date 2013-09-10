938089224913d9a394cf25fedf67ececce6ed4c4
Gonzalo Matheu <gmail@gonzalommj.com>
2013-09-10 11:12:10 -0300

    Adding vim stuff

--- start-me-up.sh
+++ start-me-up.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
+$PACKAGES="zsh tmux aptitude git guake curl openjdk-7-jre-headless maven autojump geany pidgin tree htop ack xclip vlc"
 sudo apt-get update > /dev/null
-sudo apt-get -y install zsh vim aptitude git guake curl openjdk-7-jre-headless maven autojump geany pidgin tree htop ack
+sudo apt-get -y install $PACKAGES
 
 # Zsh 
 wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | sh
@@ -9,17 +10,22 @@ wget --no-check-certificate https://github.com/robbyrussell/oh-my-zsh/raw/master
 mkdir --parents ~/.oh-my-zsh/custom/plugins
 cp functions.zsh ~/.oh-my-zsh/custom
 cp -r ./modules/zsh-syntax-highlighting ~/.oh-my-zsh/custom/plugins
-sed -i -e /plugins=.*/plugins=\(git git-extras rake autojump rvm vagrant rails history sublime bundler zsh-syntax-highlighting history-substring-search\)/g~/.zshrc
+sed -i -e /plugins=.*/plugins=\(git git-extras rake autojump rvm vagrant rails history sublime bundler zsh-syntax-highlighting history-substring-search debian\)/g~/.zshrc
 sed -i -e's/# DISABLE_CORRECTION/DISABLE_CORRECTION/g' ~/.zshrc
 sed -i -e's/ZSH_THEME=\".*\"/ZSH_THEME=\"blinks\"/g' ~/.zshrc
 
 # Highligting
 
 # Git
+EMAIL="gonzalommj@gmail.com"
 git config --global user.name "Gonzalo Matheu"
-git config --global user.email gmail@gonzalommj.com
+git config --global user.email $EMAIL
 git config --global core.pager ''
 git config --global color.ui true
+
+echo "Generating SSH keys"
+ssh-keygen -t rsa -C $EMAIL 
+
 
 # VBox
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
@@ -70,11 +76,44 @@ sudo sh -c "echo 'deb http://archive.canonical.com/ubuntu/ raring partner' >> /e
 sudo apt-get update > /dev/null
 sudo apt-get install skype
 
+# Vim
+sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
+libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
+libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial checkinstall &&
+sudo apt-get remove vim vim-runtime gvim vim-tiny vim-common vim-gui-common &&
+hg clone https://code.google.com/p/vim &&
+cd vim &&
+./configure --with-features=huge \
+            --enable-rubyinterp \
+            --enable-pythoninterp \
+            --with-python-config-dir=/usr/lib/python2.7-config \
+            --enable-perlinterp \
+            --enable-gui=gtk2 --enable-cscope --prefix=/usr &&
+make VIMRUNTIMEDIR=/usr/share/vim/vim74 &&
+sudo checkinstall &&
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1 &&
+sudo update-alternatives --set editor /usr/bin/vim &&
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1 &&
+sudo update-alternatives --set vi /usr/bin/vim &&
+sudo rm -rf vim
+
+git clone http://github.com/mutewinter/dot_vim.git ~/.vim &&
+cd ~/.vim &&
+rake vim:link &&
+git clone http://github.com/gmarik/vundle.git bundle/vundle &&
+vim "+mkspell ~/.vim/spell/custom.en.utf-8.add" +BundleInstall +qall &&
+
+sudo apt-get install build-essential cmake python-dev &&
+cd ~/.vim/bundle/YouCompleteMe &&
+./install.sh
+
+echo "Ubuntu"
 echo "Nautilus Nav bar"
 gsettings set org.gnome.nautilus.preferences always-use-location-entry true
 
 echo "Solarized"
-cp modules/dircolors.ansi-dark ~/.dircolors
-eval `dircolors ~/.dircolors`
-sh modules/gnome-terminal-colors-solarized/set_dark.sh
+cp modules/dircolors.ansi-dark ~/.dircolors &&
+eval `dircolors ~/.dircolors` &&
+sh modules/gnome-terminal-colors-solarized/set_dark.sh &&
 sh modules/guake-colors-solarized/set_dark.sh
+
