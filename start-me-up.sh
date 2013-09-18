07ef31acdde9018f06c7dd64f4c86f66db005c93
Gonzalo Matheu <gmail@gonzalommj.com>
2013-09-18 11:31:19 -0300

    Holding vim packages to prevent updates

--- start-me-up.sh
+++ start-me-up.sh
@@ -82,7 +82,8 @@ if ! (which vim > /dev/null)
   sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
   libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
   libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial checkinstall &&
-  sudo apt-get purge vim vim-runtime gvim vim-tiny vim-common vim-gui-common &&
+  sudo apt-get purge vim vim-runtime gvim vim-tiny vim-common vim-gui-common vim-gnome &&
+  sudo apt-mark hold vim vim-runtime gvim vim-tiny vim-common vim-gui-common vim-gnome
   hg clone https://code.google.com/p/vim &&
   cd vim &&
   ./configure --with-features=huge \
