7dbc595b225b9d97fd52dc9c506fc4bea84947d1
Gonzalo Matheu (vostro) <gonzalommj@gmail.com>
2013-10-01 23:54:00 -0300

    Moving vim to a separate file

--- start-me-up.sh
+++ start-me-up.sh
@@ -84,42 +84,7 @@ sudo sh -c "echo 'deb http://archive.canonical.com/ubuntu/ raring partner' >> /e
 sudo apt-get update > /dev/null
 sudo apt-get install skype
 
-# Vim
-if ! (which vim > /dev/null) 
-  then
-  echo "Installing Vim"
-  sudo apt-get install libncurses5-dev libgnome2-dev libgnomeui-dev \
-  libgtk2.0-dev libatk1.0-dev libbonoboui2-dev \
-  libcairo2-dev libx11-dev libxpm-dev libxt-dev python-dev ruby-dev mercurial checkinstall &&
-  sudo apt-get purge vim vim-runtime gvim vim-tiny vim-common vim-gui-common vim-gnome &&
-  sudo apt-mark hold vim vim-runtime gvim vim-tiny vim-common vim-gui-common vim-gnome
-  hg clone https://code.google.com/p/vim &&
-  cd vim &&
-  ./configure --with-features=huge \
-              --enable-rubyinterp \
-              --enable-pythoninterp \
-              --with-python-config-dir=/usr/lib/python2.7-config \
-              --enable-perlinterp \
-              --enable-gui=gtk2 --enable-cscope --prefix=/usr &&
-  make VIMRUNTIMEDIR=/usr/share/vim/vim74 &&
-  sudo checkinstall &&
-  sudo update-alternatives --install /usr/bin/editor editor /usr/bin/vim 1 &&
-  sudo update-alternatives --set editor /usr/bin/vim &&
-  sudo update-alternatives --install /usr/bin/vi vi /usr/bin/vim 1 &&
-  sudo update-alternatives --set vi /usr/bin/vim &&
-  cd .. &&
-  sudo rm -rf vim
-
-  git clone http://github.com/mutewinter/dot_vim.git ~/.vim &&
-  cd ~/.vim &&
-  rake vim:link &&
-  git clone http://github.com/gmarik/vundle.git bundle/vundle &&
-  vim "+mkspell ~/.vim/spell/custom.en.utf-8.add" +BundleInstall +qall &&
-
-  sudo apt-get install build-essential cmake python-dev &&
-  cd ~/.vim/bundle/YouCompleteMe &&
-  ./install.sh
-fi
+source $STU_HOME/vim.zsh
 
 echo "Ubuntu"
 echo "Nautilus Nav bar"
