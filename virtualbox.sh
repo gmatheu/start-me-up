4fbcbfd805d49a70ad8c5ab23853f76481d99b5b
Gonzalo Matheu (vostro) <gonzalommj@gmail.com>
2013-10-27 21:29:54 -0200

    Updating virtualbox version

--- virtualbox.sh
+++ virtualbox.sh
@@ -1,8 +1,8 @@
 #! /bin/sh
 
 wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
-sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian precise contrib" >> /etc/apt/sources.list.d/virtualbox.list'
+sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian raring contrib" > /etc/apt/sources.list.d/virtualbox.list'
 sudo apt-get update > /dev/null
-sudo apt-get -y install virtualbox-4.2
+sudo apt-get -y install virtualbox-4.3
 sudo apt-get -y install dkms
 
