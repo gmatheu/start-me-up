947275fb82a6b4a34f01fe34aaaef35c1fc0593f
Gonzalo Matheu (vostro) <gonzalommj@gmail.com>
2013-11-05 22:57:40 -0200

    Fixing echo

--- postresql.sh
+++ postresql.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-sudo sh 'echo deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main > /etc/apt/sources.list.d/pgdg.list'
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ precise-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | \
     sudo apt-key add -
 sudo apt-get update
