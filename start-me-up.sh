0d8c4e9c6f8bb0f26808bfec7edcd24b4306cb03
Gonzalo Matheu <gmatheu@thoughworks.com>
2013-11-29 15:20:15 -0200

    Extracting idea

--- start-me-up.sh
+++ start-me-up.sh
@@ -88,31 +88,7 @@ sudo apt-get install -y caffeine
 
 source $STU_HOME/eclipse.sh
 
-#IntelliJ Idea
-if ! (which idea > /dev/null)
-  then
-  if [ -f ideaIC-12.1.4.tar.gz ]; then
-    IDEA_PREFIX="idea-IC-12"
-    tar xf ideaIC-12.1.4.tar.gz &&
-    find . -name "$IDEA_PREFIX*" -type d -exec mv {} $IDEA_PREFIX \; &&
-    sudo mv $IDEA_PREFIX /opt &&
-    sudo cat > /usr/share/applications/idea.desktop <<EOF
-[Desktop Entry]
-Name=IntelliJ Idea 12
-Type=Application
-Exec=/opt/idea-IC-12/bin/idea.sh
-Terminal=false
-Icon=/opt/idea-IC-12/bin/idea.png
-Comment=Integrated Development Environment
-NoDisplay=false
-Categories=Development;IDE
-Name[en]=Idea
-EOF
-    sudo ln -s /opt/$IDEA_PREFIX/bin/idea.sh /usr/bin/idea
-  else 
-    echo "ideaIC-12.1.4.tar.gz should be on this folder"
-  fi
-fi
+source $STU_HOME/idea.sh
 
 # Hub
 if ! (which hub > /dev/null); then
