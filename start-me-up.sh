2bdfe6d9f83168b86ca875c3fa41a1a35146434b
Gonzalo Matheu <gmatheu@thoughworks.com>
2013-12-10 17:47:33 -0200

    Adding git stable ppa

--- start-me-up.sh
+++ start-me-up.sh
@@ -7,6 +7,9 @@ if [ ! -f $STU_HOME/secrets.sh ]; then
 fi
 source $STU_HOME/secrets.sh
 
+echo "Adding ppa repositories"
+sudo add-apt-repository -y ppa:git-core/ppa
+
 $PACKAGES="subversion zsh tmux aptitude git guake curl openjdk-7-source openjdk-7-doc openjdk-7-jdk maven autojump geany pidgin tree htop ack-grep xclip vlc meld"
 sudo apt-get update > /dev/null
 sudo apt-get -y install $PACKAGES
