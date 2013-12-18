c2a78575125f0de36e4272ec7def2dd38dc689d9
Gonzalo Matheu <gmatheu@thoughworks.com>
2013-12-18 10:43:53 -0200

    Adding apt-fast

--- utilities.sh
+++ utilities.sh
@@ -7,3 +7,9 @@ sudo apt-get -y install ubuntu-tweak
 sudo add-apt-repository -y ppa:scopes-packagers/ppa &&
 sudo apt-get update &&
 sudo apt-get -y install unity-lens-utilities unity-scope-calculator
+
+sudo aptitude install preload
+
+sudo add-apt-repository -y ppa:apt-fast/stable &&
+sudo apt-get update &&
+sudo apt-get -y install apt-fast
