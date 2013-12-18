006d32d1a2a9735487f5db767831aa38004b3d31
Gonzalo Matheu <gmatheu@thoughworks.com>
2013-12-18 10:58:37 -0200

    Addin ufw to docker

--- docker.sh
+++ docker.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
+sudo apt-get update
+sudo apt-get install linux-image-extra-`uname -r`
+
 # Add the Docker repository key to your local keychain
 # using apt-key finger you can check the fingerprint matches 36A1 D786 9245 C895 0F96 6E92 D857 6A8B A88D 21E9
 sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
@@ -12,3 +15,6 @@ sudo apt-get update
 
 # install
 sudo apt-get -y install lxc-docker
+
+sudo sed -i -e's/DEFAULT_FORWARD_POLICY=\"DROP\"/DEFAULT_FORWARD_POLICY=\"ACCEPT\"/g' /etc/default/ufw
+sudo ufw reload
