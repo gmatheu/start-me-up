bb0e5c2985eb82f78b74e657629bb99aca01eb84
Gonzalo Matheu <gmatheu@thoughtworks.com>
2014-04-27 21:27:19 -0300

    Adding oh-my-zsh custom files

--- babushka-deps/shell.rb
+++ babushka-deps/shell.rb
@@ -6,7 +6,10 @@ dep 'shell', :home do
     'zsh.bin',
     'zsh shell',
     ['zshrc', 'zshenv'].map { |d|
-      'dot file'.with(target: d, home: home / 'dotfiles')
+      'dotfile.symlink'.with(dotfile: d, home: home / 'dotfiles')
+    },
+    ['functions', 'extra-init'].map { |f|
+      'oh-my-zsh custom.symlink'.with(source_dir: home / 'custom-files', custom_file: f)
     },
     'oh-my-zsh',
     'zsh-syntax-highlighting'
@@ -27,22 +30,35 @@ dep 'oh-my-zsh' do
   meet { shell `curl -L http://install.ohmyz.sh | sh` }
 end
 
+dep 'oh-my-zsh custom.symlink', :custom_file, :source_dir do
+  source File.expand_path "#{source_dir}/#{custom_file}.zsh"
+  target File.expand_path "#{OH_MY_ZSH_CUSTOM}/#{custom_file}.zsh"
+end
+
 dep 'zsh shell' do
   requires 'zsh.bin'
   met? { shell("sudo su - '#{current_username}' -c 'echo $SHELL'") == which('zsh') }
   meet { sudo "chsh -s '#{which('zsh')}' #{current_username}" }
 end
 
-dep 'dot file', :target, :dest, :home do
-  dest.default! '~'
-  source_file = File.expand_path "#{home}/#{target}"
-  dot_file = File.expand_path "#{dest}/.#{target}"
+dep 'dotfile.symlink', :dotfile, :destination, :home do
+  destination.default! '~'
+  source File.expand_path "#{home}/#{dotfile}"
+  target File.expand_path "#{destination}/.#{dotfile}"
+end
 
-  met? { File.exists?(dot_file) && File.symlink?(dot_file) }
-  meet {
-    File.rename dot_file, "#{dot_file}.stu" if File.exists?(dot_file)
-    File.symlink source_file, dot_file
-  }
+meta :symlink do
+    accepts_value_for :source
+    accepts_value_for :target
+
+    template {
+      met? { File.exists?(target) && File.symlink?(target) }
+      meet {
+        log "Creating symlink: #{target}"
+        File.rename target, "#{target}.stu" if File.exists?(target)
+        File.symlink source, target
+      }
+    }
 end
 
 dep 'zsh.bin'
---
+++ custom-files/functions.zsh
@@ -0,0 +1,93 @@
+#! /usr/bin/zsh
+
+alias zsh_reload='source ~/.zshrc'
+alias zshr='zsh_reload'
+
+alias glo='git log --oneline --max-count=20 --reverse'
+compdef _git glo=git-log
+alias glof='git log --pretty=format:"%C(yellow)%h%Creset %s %Cred%an%Creset %Cgreen%ad" --max-count=15 --date=relative --reverse'
+compdef _git glof=git-log
+alias gstl='git stash && git pull --rebase'
+
+# Start an HTTP server from a directory, optionally specifying the port
+ function server() {
+     local port="${1:-8000}"
+     sleep 1 && open "http://localhost:${port}/" &
+     # Set the default Content-Type to `text/plain` instead of `application/octet-stream`
+     # And serve everything as UTF-8 (although not technically correct, this doesn’t break anything for binary files)
+     python -c $'import SimpleHTTPServer;\nmap = SimpleHTTPServer.SimpleHTTPRequestHandler.extensions_map;\nmap[""] = "text/plain";\nfor key, value in map.items():\n\tmap[key] =     value + ";charset=UTF-8";\nSimpleHTTPServer.test();' "$port"
+ }
+
+ # Grep aliases
+ function grep-aliases()
+ {
+ 	alias | grep $1
+ }
+
+# Grep rails routes
+function grep-rails-routes()
+ {
+ 	rake routes | grep $1
+ }
+
+alias ga="grep-aliases"
+alias rrg="grep-rails-routes"
+alias rt="rake -T"
+alias psg="ps aux | grep $@"
+alias ack="ack-grep"
+
+alias xc='xclip -selection clipboard'
+alias xp='xclip'
+alias xs='xclip -selection secondary'
+alias xo='xclip -o'
+alias xoc='xclip -o -selection clipboard'
+alias xos='xclip -o -selection secondary'
+
+alias save='echo "$@" >> ~/.saved'
+
+# Tmux
+alias txc='tmux show-buffer | xc' #Put Tmux buffer in the clipboard
+
+#Sublime plugin fix 
+if [[ $('uname') == 'Linux' ]]; then
+        if [ -f '/usr/bin/subl' ]; then
+                st_run() { nohup /usr/bin/subl $@ > /dev/null & }
+        else
+                st_run() { nohup /usr/bin/subl $@ > /dev/null & }
+        fi
+        alias st=st_run
+fi
+
+alias stt='st .'
+
+unalias ag
+
+if (which docker > /dev/null)
+then
+  alias docker='sudo docker'
+fi
+
+if (which memcached > /dev/null)
+then
+  alias clear_memcached='echo "flush_all" | nc localhost 11211'
+  alias mccl='clear_memcached'
+  alias mcc='telnet localhost 11211'
+fi
+
+alias stu="cd ~/.start-me-up"
+alias o="xdg-open"
+
+function notify() {
+  local result=$?
+  if which notify-send &> /dev/null; then
+    local name=${1}
+    if test $result -eq 0; then
+      local notification="Succeeded"
+      notify-send -t 1000 -i dialog-ok "${name} ${notification}"
+    else
+      local notification="Failed"
+      notify-send -t 3000 -i dialog-error "${name} ${notification}"
+    fi
+  fi
+}
+alias n=notify
