c593dcd2b131d7919842d551bd38d78ba18128be
Gonzalo Matheu <gmatheu@thoughtworks.com>
2014-05-13 17:55:32 -0300

    Adding missing things

--- babushka-deps/ant-downloader.rb
+++ babushka-deps/ant-downloader.rb
@@ -10,3 +10,6 @@ end
 dep 'python-pip.lib'
 dep 'libusb-1.0-0.lib'
 dep 'python-lxml.lib'
+
+# Add configuration file ~/.antd/antd.cfg
+# Add /etc/udev/rules.d/99-garmin.rules with SUBSYSTEM=="usb", ATTR{idVendor}=="0fcf", ATTR{idProduct}=="1008", MODE="666"
