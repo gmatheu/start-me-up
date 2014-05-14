ea2fc4489e1e9750fabec1850135669e99df9797
Gonzalo Matheu <gmatheu@thoughtworks.com>
2014-05-14 14:19:50 -0300

    Adding apm packages to atom

--- babushka-deps/atom.rb
+++ babushka-deps/atom.rb
@@ -1,9 +1,30 @@
+APMS = [
+  'xml-formatter',
+  'monokai',
+  'language-haml'
+]
+
 dep 'atom' do
   requires 'atom.managed'
+  requires APMS.map { |p| "#{p}" }
 end
 
 dep 'atom.managed' do
   requires 'ppa'.with('ppa:webupd8team/atom')
   installs 'atom'
-  # provides 'atom >= 0.9.4'
+  provides 'atom', 'apm'
 end
+
+APMS.each {|p| dep p, template: 'apm'}
+
+meta :apm do
+    template {
+      requires 'atom.managed'
+      met? { shell? "apm list | grep '#{name}'" }
+      meet {
+        log "Installing #{name} apm package"
+        shell "apm install #{name}"
+      }
+    }
+end
+
