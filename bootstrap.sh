fefa91bb863d711157c5794195d4809bd875ffd3
Gonzalo Matheu <gonzalommj@gmail.com>
2014-06-18 14:53:17 -0300

    Adding vars prompts. Checking in babushka is already installed

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,14 +10,27 @@ fi
 
 if [ ! -d $STU ]; 
 then
+  echo 'Cloning repository'
   which git >/dev/null 2>&1 && \
   git clone https://github.com/gmatheu/start-me-up.git $STU || {
     echo 'Could not clone repository'
     exit 1
   }
+else
+  echo 'Updating Repository'
+  cd $STU && \
+    git pull origin master
 fi
 
-sudo sh -c "`curl -s https://babushka.me/up/master`" </dev/null
+which babushka >/dev/null 2>&1 && \
+echo 'Babushka already installed' || {
+ sudo sh -c "`curl -s https://babushka.me/up/master`" </dev/null  
+}
+
+[ -z "${FULL_NAME}" ] && \
+  echo -n 'Full Name: ' && read FULL_NAME
+[ -z "${EMAIL}" ] && \
+  echo -n 'E-mail: ' && read EMAIL
 
 echo 
 echo 'Starting up...'
