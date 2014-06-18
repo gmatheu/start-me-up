77c78b01f63a5ce56734e4835fa9bb7eb15b0db4
Gonzalo Matheu <gonzalommj@gmail.com>
2014-06-18 16:42:58 -0300

    Refactoring ruby deps

--- babushka-deps/ruby.rb
+++ babushka-deps/ruby.rb
@@ -1,23 +1,46 @@
 dep 'ruby' do
-  requires 'rvm', 'ruby.bin', 'default gems'
+  requires 'rvm load', 'ruby.bin', 'rvm ruby', 'default gems'
 end
 
 dep 'ruby.bin' do
-  requires 'rvm'
-  provides 'ruby >= 1.9.3'
-  meet {
-    shell 'rvm install 2.1.1'
-    shell 'rvm use --default 2.1.1'
-  }
+  provides 'ruby >= 1.9.3p484'
 end
+
 dep 'rvm' do
   met? { which('rvm') }
 
   meet {
     shell 'bash -c "`curl -sL https://get.rvm.io`"'
     shell 'bash -c "source ~/.rvm/scripts/rvm; rvm notes"'
-    # profile = Dir["~/.bash_profile", "~/.bashrc"].first || "~/.bash_profile"
-    # shell "echo 'if [[ -s \"$HOME/.rvm/scripts/rvm\" ]] ; then source \"$HOME/.rvm/scripts/rvm\" ; fi' >> #{profile}"
+  }
+end
+dep 'rvm ruby' do
+  rvm_command = ->(cmd) {
+    "bash -c 'source ~/.rvm/scripts/rvm; rvm #{cmd}'"
+  }
+  requires 'rvm'
+  met? {
+    shell(rvm_command.call('current')) =~ /2.1.1/
+  }
+  meet {
+    shell rvm_command.call 'install 2.1.1'
+    shell rvm_command.call 'use --default 2.1.1'
+  }
+end
+
+dep 'rvm load' do
+  requires 'rvm'
+  profile = File.expand_path '~/.profile'
+  sentence = '[ -s ~/.rvm/scripts/rvm ] && source ~/.rvm/scripts/rvm'
+  met? {
+    File.open(profile).lines.any? do |line|
+      line.include? sentence
+    end
+  }
+  meet {
+    File.open(profile, 'a') do |f|
+      f.puts sentence
+    end
   }
 end
 
