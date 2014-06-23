dc948b3a4c02309717c576502ed272d91f8f895e
Gonzalo Matheu <gmatheu@thoughtworks.com>
2014-06-23 14:32:17 -0300

    Adding java aliases

--- custom-files/functions.zsh
+++ custom-files/functions.zsh
@@ -97,3 +97,10 @@ function start-me-update(){
 	echo "Updating start-me-up..."
 	cd $STU_HOME; sh bootstrap.sh 
 }
+
+
+#Java
+alias use-java-6='export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64'
+alias use-java-7='export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64'
+alias use-java-7-oracle='export JAVA_HOME=/usr/lib/jvm/java-7-oracle'
+alias use-java-8-oracle='export JAVA_HOME=/usr/lib/jvm/java-8-oracle'
