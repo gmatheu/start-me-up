52705cd1a11315f5e23962cc30ce667544a9fdce
Gonzalo Matheu <gonzalommj@gmail.com>
2014-06-29 21:55:30 -0300

    Organizing functions

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,50 +3,67 @@
 LOG_FILE='last_run.log'
 echo "Starting you up: `date`" > $LOG_FILE
 
-sudo apt-get update
-sudo apt-get -y upgrade
-sudo apt-get install -y curl git
-
 if [ ! -n "$STU" ]; then
-  STU=~/.start-me-up
+  export STU=~/.start-me-up
 fi
 
-if [ ! -d $STU ];
-then
-  echo 'Cloning repository'
-  which git >/dev/null 2>&1 && \
-  git clone https://github.com/gmatheu/start-me-up.git $STU || {
-    echo 'Could not clone repository'
-    exit 1
-  }
-else
-  echo 'Updating Repository'
-  cd $STU && \
-    git pull origin master
-fi
-
-which babushka >/dev/null 2>&1 && \
-echo 'Babushka already installed' || {
- sudo sh -c "`curl -s https://babushka.me/up/master`" </dev/null
+install_prerequesites() {
+  sudo apt-get update >/dev/null
+  sudo apt-get -y upgrade
+  sudo apt-get install -y curl git
 }
 
-[ -z "${FULL_NAME}" ] && \
-  echo -n 'Full Name: ' && read FULL_NAME
-[ -z "${EMAIL}" ] && \
-  echo -n 'E-mail: ' && read EMAIL
+get_repo() {
+  if [ ! -d $STU ];
+  then
+    echo 'Cloning repository'
+    which git >/dev/null 2>&1 && \
+    git clone https://github.com/gmatheu/start-me-up.git $STU || {
+      echo 'Could not clone repository'
+      exit 1
+    }
+  else
+    echo 'Updating Repository'
+    cd $STU && \
+      git pull origin master
+  fi
+}
 
-echo
-echo 'Starting up...'
-cd $STU; babushka start-me-up home="$STU"
+get_babushka() {
+  which babushka >/dev/null 2>&1 && \
+  echo 'Babushka already installed' || {
+   sudo sh -c "`curl -s https://babushka.me/up/master`" </dev/null
+  }
+}
 
-# Following packages are not working properly, but they can be installed using apt
-echo 'Installing remaining packages...'
+get_settings() {
+  [ -z "${FULL_NAME}" ] && \
+    echo -n 'Full Name: ' && read FULL_NAME
+  [ -z "${EMAIL}" ] && \
+    echo -n 'E-mail: ' && read EMAIL
+}
+
+run_babushka() {
+  echo
+  echo 'Starting up...'
+  cd $STU; babushka start-me-up home="$STU"
+}
+
 install_non_babushkable() {
   local dep=$1
   local packages=$2
   sudo babushka "$dep" >> $LOG_FILE 2>&1
   sudo aptitude -y install "$packages"
 }
+
+# Run!
+install_prerequesites
+get_repo
+get_babushka
+get_settings
+run_babushka
+# Following packages are not working properly, but they can be installed using apt
+echo 'Installing remaining packages...'
 install_non_babushkable 'docker' 'lxc-docker'
 install_non_babushkable 'skype' 'skype'
 install_non_babushkable 'google-chrome' 'google-chrome-stable'
