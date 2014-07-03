40b46a78195db0d46d11132f8718f0ee57f1e80f
Gonzalo Matheu <gmatheu@thoughtworks.com>
2014-07-03 13:24:58 -0300

    Adding json tool

--- babushka-deps/nodejs.rb
+++ babushka-deps/nodejs.rb
@@ -1,9 +1,15 @@
 dep 'nodejs' do
   requires 'nodejs.managed'
+  requires 'json.npm'
 end
 
 dep 'nodejs.managed' do
   requires 'ppa'.with('ppa:chris-lea/node.js')
   installs 'nodejs'
   provides 'node', 'npm'
+end
+
+dep 'json.npm' do
+  met? { which 'json'}
+  meet { sudo 'npm install -g json' }
 end
