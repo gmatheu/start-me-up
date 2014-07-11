7a82ed27a84fcbed08b870a54b999366d3d7448f
Gonzalo Matheu <gmatheu@thoughtworks.com>
2014-07-11 18:22:13 -0300

    Adding bulk dep helper

--- babushka-deps/latex.rb
+++ babushka-deps/latex.rb
@@ -7,15 +7,9 @@ texlive-latex-recommended
 
 dep 'latex' do
   requires 'texmaker.bin'
-  requires LATEX_PACKAGES.map {|p| "#{p}.lib" }
+  requires bin_dep(LATEX_PACKAGES)
 end
 
-LATEX_PACKAGES.each { |p| dep "#{p}.lib" }
 dep 'texmaker.bin'
-
-def deps_for names, suffix
-  names.each do |n|
-    dep "#{n}.#{suffix}"
-  end
-end
+lib_for LATEX_PACKAGES
 
--- babushka-deps/start-me-up.rb
+++ babushka-deps/start-me-up.rb
@@ -1,15 +1,41 @@
-PACKAGES = %Q|
+module BulkDepsHelpers
+  def deps_for names, suffix
+    names.each do |n|
+      dep "#{n}.#{suffix}"
+    end
+  end
+
+  def lib_for names
+    deps_for names, 'lib'
+  end
+
+  def bin_for names
+    deps_for names, 'bin'
+  end
+
+  def bin_dep names
+    names.map {|p| "#{p}.lib" }
+  end
+end
+
+Object.send :include, BulkDepsHelpers
+
+BASE_PACKAGES = %Q|
 subversion aptitude tig curl autojump geany pidgin
 tree htop ack-grep xclip vlc meld pass
-|.split.each { |p| dep "#{p}.lib" }
+|.split
+lib_for BASE_PACKAGES
 
-BINARIES = %Q|
+BASE_BINARIES = %Q|
 |.split.each { |p| dep "#{p}.bin" }
+bin_for BASE_BINARIES
 
 dep 'start-me-up', :home do
   home.default! '~/.start-me-up'
 
-  PACKAGES.each  { |p| requires "#{p}.lib"}
+  requires bin_dep BASE_PACKAGES
+  requires bin_dep BASE_BINARIES
+
   ['shell', 'tmux'].each do |d|
     requires d.with(home: home)
   end
@@ -21,3 +47,4 @@ dep 'start-me-up', :home do
   requires 'ant-downloader', 'caffeine', 'shutter'
   # requires 'docker', 'virtualbox', 'google-chrome', 'skype'
 end
+
