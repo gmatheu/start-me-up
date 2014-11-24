8cc775888501fa27f79fa997a36c5f028fd38eb4
Gonzalo Matheu <gonzalommj@gmail.com>
2014-11-24 11:06:41 -0200

    vagrant to 1.6.5

--- babushka-deps/vagrant.rb
+++ babushka-deps/vagrant.rb
@@ -1,6 +1,6 @@
 dep 'vagrant' do
   requires 'deb package'.with(
-    url: 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.3_x86_64.deb',
+    url: 'https://dl.bintray.com/mitchellh/vagrant/vagrant_1.6.5_x86_64.deb',
     temp_file: 'vagrant_1.6.3_x86_64.deb',
     exec: 'vagrant'
   )
