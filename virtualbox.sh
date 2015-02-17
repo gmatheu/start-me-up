707e0adc00437e7b4dadbeeaab8d525bbc62bc5b
Gonzalo Matheu <gonzalommj@gmail.com>
2015-02-17 20:56:17 -0300

    Removing old scripts

--- virtualbox.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/sh
-
-wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
-sudo sh -c 'echo "deb http://download.virtualbox.org/virtualbox/debian saucy contrib" > /etc/apt/sources.list.d/virtualbox.list'
-sudo apt-get update > /dev/null
-sudo apt-get -y install virtualbox-4.3
-sudo apt-get -y install dkms
-
