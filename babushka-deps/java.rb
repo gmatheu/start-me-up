e7dde9375ddb404f4507145712dfc84e0e552271
Gonzalo Matheu <gonzalommj@gmail.com>
2015-03-08 22:39:44 -0300

    Adding oracle java 9 and openjdk 8

--- babushka-deps/java.rb
+++ babushka-deps/java.rb
@@ -7,7 +7,7 @@ maven
 
 dep 'java' do
   requires JAVA_PACKAGES
-  requires 'oracle java.managed gradle.managed'
+  requires 'oracle java.managed', 'openjdk.managed', 'gradle.managed'
 end
 
 dep 'oracle java.managed' do
@@ -15,6 +15,15 @@ dep 'oracle java.managed' do
   installs []
   log 'You can install: oracle-java7-installer'
   log 'You can install: oracle-java8-installer'
+  log 'You can install: oracle-java9-installer'
+end
+
+dep 'openjdk.managed' do
+  requires 'ppa'.with('ppa:openjdk-r/ppa')
+  installs ['openjdk-8-jdk']
+  met? do
+    File.exists? '/usr/lib/jvm/java-1.8.0-openjdk-amd64/bin/java'
+  end
 end
 
 JAVA_PACKAGES.each{ |p| dep p }
