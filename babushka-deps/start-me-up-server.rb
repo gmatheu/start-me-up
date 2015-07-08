518931ee27337498758d9c3dff59f8576faf8963
Gonzalo Matheu <gonzalommj@gmail.com>
2015-07-07 22:17:37 -0300

    Adding start-me-up-server (copied from start-me-up dep)

---
+++ babushka-deps/start-me-up-server.rb
@@ -0,0 +1,44 @@
+module BulkDepsHelpers
+  def deps_for names, suffix
+    names.each do |n|
+      dep "#{n}.#{suffix}"
+    end
+  end
+
+  def lib_for names
+    deps_for names, 'lib'
+  end
+
+  def bin_for names
+    deps_for names, 'bin'
+  end
+
+  def bin_dep names
+    names.map {|p| "#{p}.lib" }
+  end
+end
+
+Object.send :include, BulkDepsHelpers
+
+BASE_PACKAGES = %Q|
+subversion aptitude tig curl autojump tree htop ack-grep pass libxml2-utils
+silversearcher-ag
+|.split
+lib_for BASE_PACKAGES
+
+BASE_BINARIES = %Q|
+|.split.each { |p| dep "#{p}.bin" }
+bin_for BASE_BINARIES
+
+dep 'start-me-up', :home do
+  home.default! '~/.start-me-up'
+
+  requires bin_dep BASE_PACKAGES
+  requires bin_dep BASE_BINARIES
+
+  ['shell', 'tmux'].each do |d|
+    requires d.with(home: home)
+  end
+  requires 'ruby', 'git', 'vim'
+end
+
---
+++ bootstrap-server.sh
@@ -0,0 +1,95 @@
+#! /bin/sh
+
+NC='\033[0m'
+error() {
+  color='\033[0;31m'
+  echo "${color}$1${NC} "
+} 
+info() {
+  color='\033[0;32m'
+  echo "${color}$1${NC} "
+} 
+
+LOG_FILE='last_run.log'
+echo "Starting you up: `date`" > $LOG_FILE
+
+if [ ! -n "$STU_HOME" ]; then
+  export STU_HOME="$HOME/.start-me-up"
+fi
+
+install_prerequesites() {
+  sudo apt-get update >/dev/null
+  sudo apt-get -y upgrade
+  sudo apt-get install -y curl git
+}
+
+get_repo() {
+  if [ ! -d $STU_HOME ];
+  then
+    echo 'Cloning repository'
+    which git >/dev/null 2>&1 && \
+    git clone https://github.com/gmatheu/start-me-up.git $STU_HOME || {
+      echo 'Could not clone repository'
+      exit 1
+    }
+  else
+    echo 'Updating Repository'
+    cd $STU_HOME && \
+      git pull origin master
+  fi
+}
+
+get_babushka() {
+  which babushka >/dev/null 2>&1 && \
+  echo 'Babushka already installed' || {
+   sudo sh -c "`curl -s https://babushka.me/up/master`" </dev/null
+  }
+  sudo babushka babushka 
+}
+
+get_settings() {
+  export FULL_NAME=`git config --global user.name` && \
+  [ -z "${FULL_NAME}" ] && \
+    echo -n 'Full Name: ' && read FULL_NAME && export FULL_NAME
+  export EMAIL=`git config --global user.email` && \
+  [ -z "${EMAIL}" ] && \
+    echo -n 'E-mail: ' && read EMAIL && export EMAIL
+}
+
+run_babushka() {
+  echo
+  echo 'Starting up...'
+  cd $STU_HOME; babushka start-me-up-server home="$STU_HOME"
+}
+
+install_non_babushkable() {
+  local dep=$1
+  local packages=$2
+  sudo babushka "$dep" >> $LOG_FILE 2>&1
+  sudo aptitude -y install "$packages"
+}
+
+post_install() { 
+  if (type update-vim-plugins | grep -e 'function' > /dev/null); then
+    update-vim-plugins
+  else
+    error "Run update-vim-plugins to update vim plugins"
+  fi
+  if (type zshr | grep -e 'alias' > /dev/null); then
+    zshr
+  else
+    error "Reload zsh configuration! Run zshr"  
+  fi
+}
+
+# Run!
+info "Getting latest changes"
+get_repo
+info 'Installing system packages'
+install_prerequesites
+info 'Installing babushka packages'
+get_babushka
+get_settings
+run_babushka
+post_install
+
