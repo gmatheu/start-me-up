d88993c8229682e6ca51e776e8140c1ae350bfcc
Gonzalo Matheu <gonzalommj@gmail.com>
2016-01-16 12:18:46 -0300

    Using vagrant 1.8.1

--- babushka-deps/vagrant.rb
+++ babushka-deps/vagrant.rb
@@ -2,7 +2,7 @@ dep 'vagrant' do
   version=VAGRANT_VERSION
   file = "vagrant_#{version}_x86_64.deb"
   requires 'deb package'.with(
-    url: "https://dl.bintray.com/mitchellh/vagrant/#{file}",
+    url: "https://releases.hashicorp.com/vagrant/#{version}/#{file}",
     temp_file: file,
     exec: 'vagrant',
     version: version
