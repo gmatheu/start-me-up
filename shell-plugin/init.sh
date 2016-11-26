ad72fe25ea8094a3ce5c73605f00538d22ba11e5
Gonzalo Matheu <gonzalommj@gmail.com>
2016-11-26 12:43:23 -0300

    Adding virtualbox function

--- shell-plugin/init.sh
+++ shell-plugin/init.sh
@@ -12,3 +12,12 @@ function install-babushka() {
   mkdir -p $HOME/bin
   ln -s $BABUSHKA_HOME/bin/babushka.rb $HOME/bin/babushka
 } 
+
+function stu-install-virtualbox() {
+	wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+	wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+  DIST=`lsb_release -s -c`
+	echo "deb http://download.virtualbox.org/virtualbox/debian $DIST contrib" | sudo tee /etc/apt/sources.list.d/virtualbox.list
+	sudo apt-get update
+	sudo apt-get install virtualbox-5.1 dkms
+}
