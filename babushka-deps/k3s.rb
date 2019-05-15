4f1192aae882de318713ede0e08fb3cb79d6ba33
Gonzalo Matheu <gmatheu@simtlix.com>
2019-05-15 09:49:57 -0300

    Adding k3s dep

---
+++ babushka-deps/k3s.rb
@@ -0,0 +1,10 @@
+dep 'k3s' do
+  met? {
+    in_path? 'k3s >= 0.5.0' and
+    sudo('k3s kubectl get node') =~ /Ready/
+  }
+  meet {
+    log "Installing from https://github.com/rancher/k3s/blob/master/README.md".colorize('blue')
+    sudo 'curl -sfL https://get.k3s.io | sh -'
+  }
+end
