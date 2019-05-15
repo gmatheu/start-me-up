64b902e6d30bf0ef0c634db2656122539396a1c9
Gonzalo Matheu <gmatheu@simtlix.com>
2019-05-15 09:51:49 -0300

    Parameterizing k3s dep

--- babushka-deps/k3s.rb
+++ babushka-deps/k3s.rb
@@ -1,6 +1,7 @@
-dep 'k3s' do
+dep 'k3s', :version do
+  version.default!('0.5.0')
   met? {
-    in_path? 'k3s >= 0.5.0' and
+    in_path? "k3s >= #{version}" and
     sudo('k3s kubectl get node') =~ /Ready/
   }
   meet {
